a44daf0b5ed3a4c9bc4456cd0b540cd82f19b4d4
Adam Aboelmatty <adamaboelmatty@Adams-MacBook-Pro.local>
2024-11-02 19:03:41 -0400

    “Updated”

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import React from "react";
 import { Navbar } from "./(components)/navbar";
 import { Footer } from "./(components)/footer";
 import Home from "./home";
-import EligibilityCheck from "./eligibility-check";
-import LiveCreatorAgents from "./live-creator-agents";
-import LiveCreators from "./live-creators";
+import EligibilityCheck from "./apply/page";
+import LiveCreatorAgents from "./live-creator-agents/live-creator-agents";
+import LiveCreators from "./live-creators/live-creators";
 
 export default function Layout() {
   const [currentPage, setCurrentPage] = React.useState("home");
