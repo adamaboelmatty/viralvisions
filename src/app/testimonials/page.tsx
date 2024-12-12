902ff7f678f1d48abaf4dbe57aa39975b3879aff
Adam Aboelmatty <adamaboelmatty@Adams-MacBook-Pro.local>
2024-12-11 21:47:39 -0500

    “Updated”

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -25,7 +25,7 @@ const TestimonialsPage = () => {
 
     return (
         <div style={{ width: "100%", maxWidth: "1200px", margin: "0 auto", padding: "20px" }}>
-            <h1>Wall of Love</h1>
+            <h1></h1>
             <iframe
                 id="testimonialto-viralvisions-tag-all-light"
                 src="https://embed-v2.testimonial.to/w/viralvisions?theme=light&card=small&loadMore=on&initialCount=20&randomize=on&tag=all"
